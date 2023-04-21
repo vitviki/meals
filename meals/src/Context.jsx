8e1c8fc9ef246b899c82810356cd13465978043e
vitviki <vtyagi1991@gmail.com>
2023-04-21 21:46:24 +0530

    fetch and display meals

--- meals/src/Context.jsx
+++ meals/src/Context.jsx
@@ -1,8 +1,43 @@
-const Context = () => {
+import React, { useContext, useEffect, useState } from "react"
+import axios from 'axios';
+
+const AppContext = React.createContext()
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
+
+const AppProvider = ({ children }) => {
+
+    const [ meals, setMeals ] = useState([])
+
+    const fetchMeals = async (url) => {
+        try {
+            const { data } = await axios.get(url)
+            if (data.meals) {
+                setMeals(data.meals)
+            } else {
+                setMeals([])
+            }
+        } catch (e) {
+            console.log(e.response)
+        }
+    }
+
+    useEffect(() => {
+        fetchMeals(allMealsUrl)
+    }, [])
+
 
     return (
-        <></>
+        <AppContext.Provider
+            value={{ meals }}
+        >
+            {children}
+        </AppContext.Provider>
     )
 }
 
-export default Context
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
--- meals/src/Meals.jsx
+++ meals/src/Meals.jsx
@@ -1,9 +1,25 @@
+import { useGlobalContext } from "./Context"
+import { BiLike } from "react-icons/bi"
+
 const Meals = () => {
 
+    const { meals } = useGlobalContext()
     return (
-        <div>
-            Meals
-        </div>
+        <section className="section-center">
+        {
+            meals.map((meal) => {
+                const { idMeal, strMeal: title, strMealThumb: image} = meal
+                return <article key={idMeal} className="single-meal">
+                    <img src={image} className="img" />
+                    <footer>
+                        <h5>{title}</h5>
+                        <button className="like-btn" onClick={{}}><BiLike /></button>
+                    </footer>
+                </article>
+
+            })
+        }
+        </section>
     )
 }
 
